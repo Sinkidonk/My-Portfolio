99372da686f65aac5d8f3b2ce5326f6b255d69f8
Alex Parys <Sinkidonk@users.noreply.github.com>
2020-07-24 10:00:10 -0500

    updated about me text

--- index.php
+++ index.php
@@ -15,16 +15,21 @@ require 'inc_header.php';
     </div>
     <div class="col-sm">
         <h1>Welcome to my Portfolio Website</h1>
-        <p> This website will serve as a platform to host all of my old web projects from Gateway Technical College.<br>
-Not only will this website showcase all of my web project's but it will serve to showcase all of my non-web projects as well.<br>
+        <p>
+Well hello there,<br><br>
+You found the little place where I call home on the worldwide web.<br><br>
+See that picture it hard to miss it the only picture on this page, that is me.  New Face Mask Selfie.<br><br>
+If you are an employer or a job recruiter and don’t care about who I am great check out my feature page. <br><br>
+There you will find all of the projects I am proud to show off because I actually had to put time and effort into creating them and not just copy stuff out of a book. <br><br>
+If you are anybody else, I guess you want to know who I am <br><br>
+I am Alex Parys, but you already knew that. I graduated from Gateway Technical College with a two-associate degree in IT Web Developer and IT Cybersecurity Specialist with District Honors, and Presidents Honors respectively.<br><br>
+Starting in the fall I will be working on my bachelor’s degree at UWW-Whitewater as an Information Technology BBA Major, with a Networking and Security Emphasis, and a minor in Cybersecurity.<br><br>
+My career interests lie heavily in the Cybersecurity space, as you might have been able to tell.<br><br>
+In my free time, I enjoy playing Dungeons and Dragons and being lazy while watching Netflix.<br><br>
+Well, I hoped I didn’t bore you with my intro and welcome to my home.
 
-All of the source code for my projects will be posted on GitHub, however, due to the nature of GitHub I will be omitting any database information, username, or password to the code that gets uploaded where needed.<br>
 
-So, hi I am Alex Parys, I graduated from Gateway Technical College with a duel Associate degree in IT Web Developer, and IT Cybersecurity Specialist, on 8/18, and 4/20 respectively. <br>
-
-Starting in the fall I will be working on my bachelor degree at UWW-Whitewater as an Information Technology BBA Major, with an Networking and Security Emphasis, and a minor in Cybersecurity.
-
-</p>
+        </p>
         
         </div>
     
