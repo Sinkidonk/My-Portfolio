d9df3083903fadc587e38db85b2239f961ac3bae
Alex Parys <Sinkidonk@users.noreply.github.com>
2020-07-24 09:12:16 -0500

    updated to include the my-profile image

--- index.php
+++ index.php
@@ -4,8 +4,18 @@ require 'inc_header.php';
 
 ?>
 
-<h1>Welcome to my Portfolio Website</h1>
-<p> This website will serve as a platform to host all of my old web projects from Gateway Technical College.<br>
+<div class="container">
+    
+    <div class="row">
+        
+    <div class="col-sm">
+        <div style="width:85%;height:20rem;">
+        <img src="img/my-profile.jpg" alt="Alex Parys" class="img-fluid float-left">
+            </div>
+    </div>
+    <div class="col-sm">
+        <h1>Welcome to my Portfolio Website</h1>
+        <p> This website will serve as a platform to host all of my old web projects from Gateway Technical College.<br>
 Not only will this website showcase all of my web project's but it will serve to showcase all of my non-web projects as well.<br>
 
 All of the source code for my projects will be posted on GitHub, however, due to the nature of GitHub I will be omitting any database information, username, or password to the code that gets uploaded where needed.<br>
@@ -15,7 +25,14 @@ So, hi I am Alex Parys, I graduated from Gateway Technical College with a duel A
 Starting in the fall I will be working on my bachelor degree at UWW-Whitewater as an Information Technology BBA Major, with an Networking and Security Emphasis, and a minor in Cybersecurity.
 
 </p>
+        
+        </div>
+    
+    
+    </div>
 
+
+</div>
 
 
 
