70082d143c0812cc6938780d4a4a283586daa863
Alex Parys <Sinkidonk@users.noreply.github.com>
2020-07-26 02:26:47 -0500

    Update inc_header.php

--- inc_header.php
+++ inc_header.php
@@ -28,7 +28,7 @@
         </header>
         
         <nav class="nav nav-pills nav-fill">
-            <a class="nav-item nav-link active" href="#">Active</a>
+            <a class="nav-item nav-link active" href="./index.php">Home</a>
             <a class="nav-item nav-link" href="#">Link</a>
             <a class="nav-item nav-link" href="#">Link</a>
             <a class="nav-item nav-link disabled" href="#">Disabled</a>
