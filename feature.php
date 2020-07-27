9ca9078d2f5a4881b2d7dd8b8b21daf50aeef780
Alex Parys <Sinkidonk@users.noreply.github.com>
2020-07-27 17:59:21 -0500

    Created base layout
    Created the base layout for the Feature page, border added to verify layout is correct.

--- feature.php
+++ feature.php
@@ -2,8 +2,34 @@
 
 $pageTitle = "Feature";
 require 'inc_header.php';
-
 ?>
+
+<div class="container">
+    <div class="row mw-100">
+    <div class="col-12 border">
+                    <h1>About Feature Page</h1>
+        <p>This page will feature all of the stuff I worked on that required actual efforts from me to work on.<br>
+            My final project’s as well as personal projects, anything that wasn’t just straight out of a book where it was like oh do this and then does that, oh finds and correct these errors in the code. Type of deal.
+        </p>
+        </div>
+    </div>
+    <!-- TODO: Create php code to automagically fill stuff out from a database -->
+<div class="row mw-100">
+    <div class="col-12 border">
+        Name of Project<br>
+        About Project
+    </div>
+    </div>
+    <div class="row mw-100">
+    <div class="col-6 border">
+        Download/View in web
+    </div>
+    <div class="col-6 border">
+        View Code
+        </div>
+</div>
+</div>
+
 
 
 
@@ -11,4 +37,8 @@ require 'inc_header.php';
 
 <?php 
 require 'inc_footer.php';
- ?>
+?>
+
+      
+      
+      
