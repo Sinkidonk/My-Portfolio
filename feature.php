b7364b6ba543c2bc308012201129cf13723b3e61
Alex Parys <Sinkidonk@users.noreply.github.com>
2020-07-27 18:11:24 -0500

    Fixed spelling error

--- feature.php
+++ feature.php
@@ -9,7 +9,7 @@ require 'inc_header.php';
     <div class="col-12 border">
                     <h1>About Feature Page</h1>
         <p>This page will feature all of the stuff I worked on that required actual efforts from me to work on.<br>
-            My final project’s as well as personal projects, anything that wasn’t just straight out of a book where it was like oh do this and then does that, oh finds and correct these errors in the code. Type of deal.
+            My final project’s as well as personal projects, anything that wasn’t just straight out of a book where it was like oh do this and then do that, oh finds and correct these errors in the code. Type of deal.
         </p>
         </div>
     </div>
