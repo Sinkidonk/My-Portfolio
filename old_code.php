0d8ee42c043ec96f2c8a2c84bc329876d4294e9b
Alex Parys <Sinkidonk@users.noreply.github.com>
2020-08-02 02:03:38 -0500

    begining of the end

--- old_code.php
+++ old_code.php
@@ -1,6 +1,7 @@
 <?php
 $pageTitle = "Code Archive";
 require 'inc_header.php';
+require 'loop-old-code.php';
 
 ?>
 
@@ -13,6 +14,14 @@ require 'inc_header.php';
         Just stuff like that nothing special, nothing fancy.<br>
         For web code links will be provided to view it running, however just check out my github it will all be there.
     </p>
+    
+    <?php
+    $arraylist = array("Java 1","Java 2","asp.net 1","asp.net 2");
+    
+    $elementsloop = new loop_old_code();
+    $elementsloop->set_array_values = $arraylist;
+    $elementsloop->creation();
+    ?>
     
 </div> <!-- container -->
 
