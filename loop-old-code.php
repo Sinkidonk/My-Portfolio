dfe9abb35111f9ec8449a764088be0a980b4f8d8
Alex Parys <Sinkidonk@users.noreply.github.com>
2020-08-02 01:17:43 -0500

    looper old code
    This will serve as an easy way to help me code the rest of my website as there will be a lot of repeat of code otherwise.

---
+++ loop-old-code.php
@@ -0,0 +1,25 @@
+<?php
+class loop_old_code {
+    
+    private $array_values;
+    
+    public function get_array_values() {
+        return $this->array_values;
+    }
+    
+    public function set_array_values() {
+        $this->array_values = $array_values;
+        
+    }
+    
+    
+    public function creation() {
+        /* put all of the html code here */
+    }
+    
+
+    
+
+}
+
+?>
