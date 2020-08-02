41a1e8b7a352044582fa06d19b6cf18e973c5ef7
Alex Parys <Sinkidonk@users.noreply.github.com>
2020-08-02 00:56:37 -0500

    added intro

--- old_code.php
+++ old_code.php
@@ -5,7 +5,14 @@ require 'inc_header.php';
 ?>
 
 <div class="container">
-    
+    <h1>About the Code Archive</h1>
+    <p>
+        This is where all of my less useful code will live.<br>
+        I am talking about all the code that was created due to a standard homework assigment.<br>
+        Like debug this script, draw a circle on the screen, remove all the red from this picture.<br>
+        Just stuff like that nothing special, nothing fancy.<br>
+        For web code links will be provided to view it running, however just check out my github it will all be there.
+    </p>
     
 </div> <!-- container -->
 
