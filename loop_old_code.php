f2c78bfb3ddaa550398a730ad40cd3cb56472ef6
Alex Parys <Sinkidonk@users.noreply.github.com>
2020-08-03 12:34:08 -0500

    improve looping logic
    Need more improvements done need to find a better way to pass variables into the inner loop.
    I want it to create a 4 row grid, with the input from a variable

--- loop_old_code.php
+++ loop_old_code.php
@@ -5,20 +5,40 @@
 
 */
 
+// todo: need a way to carry variable info into the inner loop.
+
+// might have to rewrite as a do while loop
+
 function tagsloop($myarray)
 {
     $output = '<div class="altcoloring">';
     
+    $math = count($myarray)/4;
     
     for ($count = 0; $count < count($myarray); $count++){
         $output .= '<div class="row mw-100">';
         // might have to use math to create loops that can do what I want
-        for ($eachcol = 0; $eachcol < count($myarray); $eachcol++){
+        for ($eachcol = 0; $eachcol < $math; $eachcol++){
             $output .= '<div class="col-sm-3">';
-            $output .= '<h4>';
+            $output .= '<p>';
             $output .= $myarray[$count][0];
-            $output .= '</h4>';
+            $output .= '</p>';
+            $output .= '<a class="text-center pb-t pt-2" target="_blank" href="';
+            $output .= $myarray[$count][2];
+            $output .= '">';
+            switch ($myarray[$count][1]) {
+                case 0:
+                    $output .= 'View in GitHub</a>';
+                    break;
+                case 1:
+                    $output .= 'View in Web</a>';
+                    break;
+                default:
+                    $output .= 'Um how did you get here</a>';
+                    
+            }
             $output .= '</div>';
+            $count++;
         }
         
         $output .= "</div>";
