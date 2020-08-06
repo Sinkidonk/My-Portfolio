974b0384c7044dee204dd2d34589931825483d9d
Alex Parys <Sinkidonk@users.noreply.github.com>
2020-08-06 17:49:59 -0500

    link to github text

--- old_code.php
+++ old_code.php
@@ -18,19 +18,19 @@ require 'loop_old_code.php';
     <?php
     
     $arraylist = array(
-        array ("Java 1", 0, "https://github.com/Sinkidonk/Java-1-Code"),
-        array ("Java 2", 0, "https://github.com/Sinkidonk/Java-2-Code"),
-        array ("Javascript", 0, "https://github.com/Sinkidonk/Javascript-Code"),
-        array ("Web Programming 1", 0, "https://github.com/Sinkidonk/Web-Programming-1-code"),
-        array ("PHP Basics", 0, "https://github.com/Sinkidonk/PHP-Basics"),
-        array ("PowerShell", 0, "https://github.com/Sinkidonk/PowerShell-Code"),
-        array ("Advance PHP", 0, "https://github.com/Sinkidonk/Advance-PHP"),
-        array ("asp.net 1", 0, "https://github.com/Sinkidonk/asp.net-1-code"),
-        array ("asp.net 2", 0, "https://github.com/Sinkidonk/asp.net-2-code"),
-        array ("Android Code", 0, "https://github.com/Sinkidonk/Android-Code"),
-        array ("Web Programming 2", 0, "https://github.com/Sinkidonk/Web-Programming-2-code"),
-        array ("High School Web Design 1", 0, "https://github.com/Sinkidonk/high-school-web-design-1"),
-        array ("Python", 0, "https://github.com/Sinkidonk/Python-Code")
+        array ("Java 1", 2, "https://github.com/Sinkidonk/Java-1-Code"),
+        array ("Java 2", 2, "https://github.com/Sinkidonk/Java-2-Code"),
+        array ("Javascript", 2, "https://github.com/Sinkidonk/Javascript-Code"),
+        array ("Web Programming 1", 2, "https://github.com/Sinkidonk/Web-Programming-1-code"),
+        array ("PHP Basics", 2, "https://github.com/Sinkidonk/PHP-Basics"),
+        array ("PowerShell", 2, "https://github.com/Sinkidonk/PowerShell-Code"),
+        array ("Advance PHP", 2, "https://github.com/Sinkidonk/Advance-PHP"),
+        array ("asp.net 1", 2, "https://github.com/Sinkidonk/asp.net-1-code"),
+        array ("asp.net 2", 2, "https://github.com/Sinkidonk/asp.net-2-code"),
+        array ("Android Code", 2, "https://github.com/Sinkidonk/Android-Code"),
+        array ("Web Programming 2", 2, "https://github.com/Sinkidonk/Web-Programming-2-code"),
+        array ("High School Web Design 1", 2, "https://github.com/Sinkidonk/high-school-web-design-1"),
+        array ("Python", 2, "https://github.com/Sinkidonk/Python-Code")
         
         
         
