94176a5d40c436ae6242b1335d702762a75db23b
Alex Parys <Sinkidonk@users.noreply.github.com>
2020-08-06 16:57:23 -0500

    created javascript page

--- javascript.php
+++ javascript.php
@@ -1,37 +1,85 @@
 <?php
-$pageTitle = "Code Archive";
+$pageTitle = "Javascript Archive";
 require 'inc_header.php';
 require 'loop_old_code.php';
 
 ?>
 
 <div class="container">
-    <h1>About the Code Archive</h1>
+    <h1>Javascript Archive</h1>
     <p>
-        This is where all of my less useful code will live.<br>
-        I am talking about all the code that was created due to a standard homework assigment.<br>
-        Like debug this script, draw a circle on the screen, remove all the red from this picture.<br>
-        Just stuff like that nothing special, nothing fancy.<br>
-        For web code links will be provided to view it running, however just check out my github it will all be there.
+        This is where all of my Javascript based code live.
+        Enjoy your stay.
     </p>
     
     <?php
     
     $arraylist = array(
-        array ("Java 1", 0, "https://github.com/Sinkidonk/Java-1-Code"),
-        array ("Java 2", 0, "https://github.com/Sinkidonk/Java-2-Code"),
-        array ("Javascript", 0, "https://github.com/Sinkidonk/Javascript-Code"),
-        array ("Web Programming 1", 0, "https://github.com/Sinkidonk/Web-Programming-1-code"),
-        array ("PHP Basics", 0, "https://github.com/Sinkidonk/PHP-Basics"),
-        array ("PowerShell", 0, "https://github.com/Sinkidonk/PowerShell-Code"),
-        array ("Advance PHP", 0, "https://github.com/Sinkidonk/Advance-PHP"),
-        array ("asp.net 1", 0, "https://github.com/Sinkidonk/asp.net-1-code"),
-        array ("asp.net 2", 0, "https://github.com/Sinkidonk/asp.net-2-code"),
-        array ("Android Code", 0, "https://github.com/Sinkidonk/Android-Code"),
-        array ("Web Programming 2", 0, "https://github.com/Sinkidonk/Web-Programming-2-code"),
-        array ("high school web design 1", 0, "https://github.com/Sinkidonk/high-school-web-design-1"),
-        array ("Python", 0, "https://github.com/Sinkidonk/Python-Code")
+        array("Unit 1 Hands on Project 1-1", 1, "./archive/javascript/u1/hands-on-project1-1/index.htm"),
+        array("Unit 1 Hands on Project 1-2", 1, "./archive/javascript/u1/hands-on-project1-2/index.htm"),
+        array("Unit 1 Hands on Project 1-3", 1, "./archive/javascript/u1/hands-on-project1-3/index.htm"),
+        array("Unit 1 Hands on Project 1-4", 1, "./archive/javascript/u1/hands-on-project1-4/index.htm"),
+        array("Unit 1 Hands on Project 1-5", 1, "./archive/javascript/u1/hands-on-project1-5/index.htm"),
         
+        array("Unit 2 Hands on Project 2-1", 1, "./archive/javascript/u2/hands-on-project2-1/index.htm"),
+        array("Unit 2 Hands on Project 2-2", 1, "./archive/javascript/u2/hands-on-project2-2/index.htm"),
+        array("Unit 2 Hands on Project 2-3", 1, "./archive/javascript/u2/hands-on-project2-3/index.htm"),
+        array("Unit 2 Hands on Project 2-4", 1, "./archive/javascript/u2/hands-on-project2-4/index.htm"),
+        array("Unit 2 Hands on Project 2-5", 1, "./archive/javascript/u2/hands-on-project2-5/index.htm"),
+        
+        array("Unit 3 Hands on Project 3-1", 1, "./archive/javascript/u3/hands-on-project3-1/index.htm"),
+        array("Unit 3 Hands on Project 3-2", 1, "./archive/javascript/u3/hands-on-project3-2/index.htm"),
+        array("Unit 3 Hands on Project 3-3", 1, "./archive/javascript/u3/hands-on-project3-3/index.htm"),
+        array("Unit 3 Hands on Project 3-4", 1, "./archive/javascript/u3/hands-on-project3-4/index.htm"),
+        array("Unit 3 Hands on Project 3-5", 1, "./archive/javascript/u3/hands-on-project3-5/index.htm"),
+        
+        array("Unit 4 Hands on Project 4-1", 1, "./archive/javascript/u4/hands-on-project4-1/tractor.htm"),
+        array("Unit 4 Hands on Project 4-2", 1, "./archive/javascript/u4/hands-on-project4-2/tractor.htm"),
+        array("Unit 4 Hands on Project 4-3", 1, "./archive/javascript/u4/hands-on-project4-3/index.htm"),
+        array("Unit 4 Hands on Project 4-4", 1, "./archive/javascript/u4/hands-on-project4-4/index.htm"),
+        array("Unit 4 Hands on Project 4-5", 1, "./archive/javascript/u4/hands-on-project4-5/index.htm"),
+        
+        array("Unit 5 Hands on Project 5-1", 1, "./archive/javascript/u5/hands-on-project5-1/index.htm"),
+        array("Unit 5 Hands on Project 5-2", 1, "./archive/javascript/u5/hands-on-project5-2/index.htm"),
+        array("Unit 5 Hands on Project 5-3", 1, "./archive/javascript/u5/hands-on-project5-3/index.htm"),
+        array("Unit 5 Hands on Project 5-4", 1, "./archive/javascript/u5/hands-on-project5-4/index.htm"),
+        array("Unit 5 Hands on Project 5-5", 1, "./archive/javascript/u5/hands-on-project5-5/index.htm"),
+        
+        array("Unit 6 Hands on Project 6-1", 1, "./archive/javascript/u6/hands-on-project6-1/index.htm"),
+        array("Unit 6 Hands on Project 6-2", 1, "./archive/javascript/u6/hands-on-project6-2/index.htm"),
+        array("Unit 6 Hands on Project 6-3", 1, "./archive/javascript/u6/hands-on-project6-3/index.htm"),
+        array("Unit 6 Hands on Project 6-4", 1, "./archive/javascript/u6/hands-on-project6-4/index.htm"),
+        array("Unit 6 Hands on Project 6-5", 1, "./archive/javascript/u6/hands-on-project6-5/index.htm"),
+        
+        array("Unit 7 Hands on Project 7-1", 1, "./archive/javascript/u7/hands-on-project7-1/index.htm"),
+        array("Unit 7 Hands on Project 7-2", 1, "./archive/javascript/u7/hands-on-project7-2/index.htm"),
+        array("Unit 7 Hands on Project 7-3", 1, "./archive/javascript/u7/hands-on-project7-3/index.htm"),
+        array("Unit 7 Hands on Project 7-4", 1, "./archive/javascript/u7/hands-on-project7-4/index.htm"),
+        array("Unit 7 Hands on Project 7-5", 1, "./archive/javascript/u7/hands-on-project7-5/index.htm"),
+        
+        array("Unit 8 Hands on Project 8-1", 1, "./archive/javascript/u8/hands-on-project8-1/index.htm"),
+        array("Unit 8 Hands on Project 8-2", 1, "./archive/javascript/u8/hands-on-project8-2/index.htm"),
+        array("Unit 8 Hands on Project 8-3", 1, "./archive/javascript/u8/hands-on-project8-3/index.htm"),
+        array("Unit 8 Hands on Project 8-4", 1, "./archive/javascript/u8/hands-on-project8-4/index.htm"),
+        array("Unit 8 Hands on Project 8-5", 1, "./archive/javascript/u8/hands-on-project8-5/index.htm"),
+        
+        array("Unit 9 Hands on Project 9-1", 1, "./archive/javascript/u9/hands-on-project9-1/index.htm"),
+        array("Unit 9 Hands on Project 9-2", 1, "./archive/javascript/u9/hands-on-project9-2/index.htm"),
+        array("Unit 9 Hands on Project 9-3", 1, "./archive/javascript/u9/hands-on-project9-3/index.htm"),
+        array("Unit 9 Hands on Project 9-4", 1, "./archive/javascript/u9/hands-on-project9-4/index.htm"),
+        array("Unit 9 Hands on Project 9-5", 1, "./archive/javascript/u9/hands-on-project9-5/index.htm"),
+        
+        array("Unit 10 Hands on Project 11-1", 1, "./archive/javascript/u10/hands-on-project11-1/search.htm"),
+        array("Unit 10 Hands on Project 11-2", 1, "./archive/javascript/u10/hands-on-project11-2/stocks.htm"),
+        array("Unit 10 Hands on Project 11-3", 1, "./archive/javascript/u10/hands-on-project11-3/stocks.htm"),
+        array("Unit 10 Hands on Project 11-4", 1, "./archive/javascript/u10/hands-on-project11-4/address.htm"),
+        array("Unit 10 Hands on Project 11-5", 1, "./archive/javascript/u10/hands-on-project11-5/address.htm"),
+        
+        array("Unit 12 Hands on Project 12-1", 1, "./archive/javascript/u12/hands-on-project12-1/index.htm"),
+        array("Unit 12 Hands on Project 12-2", 1, "./archive/javascript/u12/hands-on-project12-2/index.htm"),
+        array("Unit 12 Hands on Project 12-3", 1, "./archive/javascript/u12/hands-on-project12-3/index.htm"),
+        array("Unit 12 Hands on Project 12-4", 1, "./archive/javascript/u12/hands-on-project12-4/index.htm"),
+        array("Unit 12 Hands on Project 12-5", 1, "./archive/javascript/u12/hands-on-project12-5/index.htm")
         
         
     ); // end of array
