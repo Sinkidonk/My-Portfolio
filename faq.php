3dc38073ea71f8f3138f8f0b4c6781017693d9e8
Alex Parys <Sinkidonk@users.noreply.github.com>
2020-08-08 14:38:30 -0500

    Create faq.php

---
+++ faq.php
@@ -0,0 +1,22 @@
+<?php
+$pageTitle = "F.A.Q";
+require 'inc_header.php';
+require 'loop_old_code.php';
+
+?>
+
+<div class="container">
+    <h1>Frequently Asked Questions</h1>
+    <p>
+        If you are asking your self
+    </p>
+   
+    
+</div> <!-- container -->
+
+
+
+
+<?php 
+require 'inc_footer.php';
+ ?>
