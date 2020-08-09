2f231b725a6e497af4637a10482df3426ffe2011
Alex Parys <Sinkidonk@users.noreply.github.com>
2020-08-09 17:53:05 -0500

    Update faq.php

--- faq.php
+++ faq.php
@@ -6,10 +6,21 @@ require 'loop_old_code.php';
 ?>
 
 <div class="container">
+    
+    <!-- going to make an function with an array input to handle this -->
     <h1>Frequently Asked Questions</h1>
     <p>
-        If you are asking your self
+        If you are asking, yourself who are asking these questions well me I am. This page for now is going to be used for answering all the questions that I asked myself that I think other people would ask me.
     </p>
+    
+    <ul>
+        <li>What made you go and create this website?</li>
+        <ul>Well I am looking to get an internship when I start back at college, in the fall. This 3rd degree will be my last and I think it a good time to start trying to gain work experience.</ul>
+        <li>What you got two degrees already but in what and from where?</li>
+        <ul>I got both of my degrees from Gateway Technical College.<br>
+        One in IT Cybersecurity Specialist, and the other in IT Web Software Developer.
+        </ul>
+    </ul>
    
     
 </div> <!-- container -->
