1c85a160dc1dac7f72b423a5584347e69c5528b4
Alex Parys <Sinkidonk@users.noreply.github.com>
2020-08-16 15:08:59 -0500

    fixed up footer

--- inc_footer.php
+++ inc_footer.php
@@ -1,6 +1,7 @@
 
 <footer>
     <p>Alex Parys 2020&copy;
-        <a href="mailto:aparys001@gmail.com">aparys001@gmail.com</a>
+        <br>
+        Send me an email at <a href="mailto:aparys001@gmail.com">aparys001@gmail.com</a>
     </p>
 </footer>
