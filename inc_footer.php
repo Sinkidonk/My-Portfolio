6c8d7e55163aabf287b0d92297b6faa6d1b099d7
Alex Parys <Sinkidonk@users.noreply.github.com>
2020-08-19 14:34:01 -0500

    improve the footer

--- inc_footer.php
+++ inc_footer.php
@@ -1,7 +1,9 @@
 
 <footer>
-    <p>Alex Parys 2020&copy;
-        <br>
-        Send me an email at <a href="mailto:aparys001@gmail.com">aparys001@gmail.com</a>
-    </p>
+    <div class="mx-auto text-left">
+        Currently looking for an intership, around the area of Salem, WI.<br>
+        If you want to send me an email about possible opportunities.<br>
+        Send me an email at <a href="mailto:aparys001@gmail.com">aparys001@gmail.com</a><br>
+        Alex Parys 2020&copy;
+</div>
 </footer>
